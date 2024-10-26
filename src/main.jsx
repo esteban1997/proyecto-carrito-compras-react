c73794f642198ea6b47c3634b690bddf45bc2ac4
esteban aguirre martinez <estebanaguirre1997@gmail.com>
2024-10-25 22:47:38 -0500

    se sube proyecto inicial

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { CarritoApp } from './CarritoApp'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <StrictMode>
+      <CarritoApp />
+    </StrictMode>,
+  </BrowserRouter>
+)
